ec6379e96bc10f3bafc72c1eb9b524e2c10e7a7b
wcollins <26700607+wcollins@users.noreply.github.com>
2026-02-03 08:57:16 -0500

    feat: add barrel export for log components

---
+++ web/src/components/log/index.ts
@@ -0,0 +1,11 @@
+export { LogLine } from './LogLine';
+export { LevelFilter } from './LevelFilter';
+export { ZoomControls } from './ZoomControls';
+export {
+  type LogLevel,
+  type ParsedLog,
+  LOG_LEVELS,
+  LEVEL_STYLES,
+  parseLogEntry,
+  formatTimestamp,
+} from './logTypes';
